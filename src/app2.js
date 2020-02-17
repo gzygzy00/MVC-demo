1dea440a40d0897f3a513fdeff29ae96759d2055
gzy <283211498@qq.com>
2020-02-17 14:34:36 +0800

    add app2

--- src/app2.js
+++ src/app2.js
@@ -1,3 +1,20 @@
 import './app2.css'
 import $ from 'jquery'
 
+const $tabContent = $('.tab-content')
+const $tabBar = $('.tab-bar')
+
+$tabBar.on('click', 'li', (e)=>{
+  // console.log(e.target)   // span
+  // console.log(e.currentTarget)    // li
+  const $li = $(e.currentTarget)
+  const index = $li.index()
+
+  $li.addClass('selected')
+      .siblings().removeClass('selected')
+
+  $tabContent.children().eq(index)
+      .addClass('active')
+      .siblings().removeClass('active')
+})
+$tabBar.children().eq(0).trigger('click')
